fcfc67626caded5dbf6632b6b9669d0fe4b59831
ibrahimAnis <anisibrahim21@gmail.com>
2021-01-11 10:20:03 +0530

    assignment

--- run_celery.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-cd backend  
-# run Celery worker for our project myproject with Celery configuration stored in Celeryconf
-#"celery worker -A myproject.celeryconf -Q default -n default@%h"
-su -m myuser -c "celery -A team_wave worker -l info"
-
-
